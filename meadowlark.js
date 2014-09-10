bdec31ec95469295dddc2d1b41f9dc0da2c6bd1f
narwhaldo <designgumption@gmail.com>
2014-09-09 17:19:18 -0700

    first commit

---
+++ meadowlark.js
@@ -0,0 +1,50 @@
+var express = require('express');
+
+var app = express();
+
+// set up handlebars view engine
+var handlebars = require('express-handlebars')
+  .create({ defaultLayout: 'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/about', function(req, res){
+	res.render('about');
+});
+
+// app.get('/about/contact', function(req, res){
+// 	res.type('text/plain');
+// 	res.send('Contact Meadowlark Travel');
+// });
+
+// app.get('/about/directions', function(req, res){
+// 	res.type('text/plain');
+// 	res.send('Directions to Meadowlark Travel');
+// });
+
+// custom 404 page
+app.use(function(req, res){
+	res.status(404);
+	res.render('404');
+});
+
+// custom 500 page
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});
